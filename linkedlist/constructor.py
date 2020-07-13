716313237a7f7b345f74e5fea7cc76265a249e9b
Ruslan <ruslankotvytskyi@gmail.com>
2020-07-13 19:25:13 +0800

    Added base functions
    Added Node + DoublyLinkedList

---
+++ linkedlist/constructor.py
@@ -0,0 +1,100 @@
+
+# This is an input class. Do not edit.
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def setHead(self, node):
+        if self.head == None:
+            self.tail = node
+            self.head = node
+        else:
+            self.insertBefore(self.head, node)
+
+    def setTail(self, node):
+        if self.tail == None:
+            self.setHead(node)
+        else:
+            self.insertAfter(self.tail, node)
+
+    def insertBefore(self, node, nodeToInsert):
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.next = node
+        nodeToInsert.prev = node.prev
+        if node.prev is None:
+            self.head = nodeToInsert
+        else:
+            node.prev.next = nodeToInsert
+        node.prev = nodeToInsert
+
+    def insertAfter(self, node, nodeToInsert):
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.next = node.next
+        nodeToInsert.prev = node
+        if node.next is None:
+            self.tail = nodeToInsert
+        else:
+            node.next.prev = nodeToInsert
+        node.next = nodeToInsert
+
+    def insertAtPosition(self, position, nodeToInsert):
+        node = self.head
+        counter = 1
+        while node is not None and counter != position:
+            node = node.next
+            counter += 1
+
+        if node is not None:
+            self.insertBefore(node, nodeToInsert)
+        else:
+            self.setTail(nodeToInsert)
+
+    def removeNodesWithValue(self, value):
+        node = self.head
+        while node is not None:
+            nodeToRemove = node
+            node = node.next
+            if nodeToRemove.value == value:
+                self.remove(nodeToRemove)
+
+    def remove(self, node):
+        if node == self.head:
+            self.head = node.next
+        if node == self.tail:
+            self.tail = node.prev
+        self.removeHelperBindings(node)
+
+    def containsNodeWithValue(self, value):
+        node = self.head
+        while node is not None:
+            if node.value == value:
+                return True
+            node = node.next
+        return False
+
+    def removeHelperBindings(self, node):
+        if node.prev is not None:
+            node.prev.next = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        node.next = None
+        node.prev = None
+    # node.value = None
+
+    def printLinkedList(self):
+        node = self.head
+        while node is not None:
+            print(node.value, "<->")
+            node = node.next
