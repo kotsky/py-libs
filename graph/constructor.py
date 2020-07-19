81b1575af23d138d9c319d4daba7222bb0735883
Ruslan <ruslankotvytskyi@gmail.com>
2020-07-19 16:13:35 +0800

    Update constructor.py

--- graph/constructor.py
+++ graph/constructor.py
@@ -13,3 +13,20 @@ class Node:
         for name in self.children:
         	name.depthFirstSearch(array)
         return array
+
+	# O(v + e) T / O(v) S
+    def breadthFirstSearch(self, array):
+        i = 0
+        array.append(self)
+        length = len(array)
+        while i < length:
+            for child in array[i].children:
+                array.append(child)
+            array[i] = array[i].name
+            i += 1
+            length = len(array)
+        return array
+
+'''
+	Version 2 of BFS. Through queue + pop(0) - no that efficienct for really big N.
+'''
