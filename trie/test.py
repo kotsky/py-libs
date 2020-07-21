67a7f2fa06dcb19c74ba8b2637848282b5ac0fb9
Ruslan <ruslankotvytskyi@gmail.com>
2020-07-21 16:45:06 +0800

    Update test.py

--- trie/test.py
+++ trie/test.py
@@ -1,9 +1,8 @@
-def test_case_1(self):
-    trie = SuffixTrie("babc")
-    expected = {
+expected = {
         "c": {"*": True},
         "b": {"c": {"*": True}, "a": {"b": {"c": {"*": True}}}},
         "a": {"b": {"c": {"*": True}}},
     }
+trie = SuffixTrie("babc")
 print(trie.root)
 print(trie.contains("abc"))
