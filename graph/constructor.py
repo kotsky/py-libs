22fb5df5bee140f4863f4799a08e0d9548bb3372
Ruslan <ruslankotvytskyi@gmail.com>
2020-08-04 13:50:58 +0800

    Update constructor.py

--- graph/constructor.py
+++ graph/constructor.py
@@ -30,3 +30,44 @@ class Node:
 '''
 	Version 2 of BFS. Through queue + pop(0) - no that efficienct for really big N.
 '''
+
+# The fubctions/classes below are to
+# build graphs from list/array [1, 2, 3, 4].
+# each of those numbers (#job) has dependencies
+# as [[1, 2], [1, 3], [3, 2], [4, 2], [4, 3]] means #1 1 <- 2 etc..
+
+
+def createGraph(values, deps):
+    graph = Graph(values)
+    for dep, value in deps:
+        graph.addDep(value, dep)
+    return graph
+
+
+class Graph:
+    def __init__(self, values):
+        self.nodes = []
+        self.graph = {}
+        for value in values:
+            self.addNode(value)
+
+    def addNode(self, value):
+        self.graph[value] = GraphNode(value)
+        self.nodes.append(self.graph[value])
+
+    def getNode(self, value):
+        if value not in self.graph:
+            self.addNode(value)
+        return self.graph[value]
+
+    def addDep(self, value, dep):
+        valueNode = self.getNode(value)
+        depNode = self.getNode(dep)
+        valueNode.dependencies.append(depNode)
+
+
+class GraphNode:
+    def __init__(self, value):
+        self.value = value
+        self.dependencies = []
+        
